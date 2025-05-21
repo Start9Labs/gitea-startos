3cf8ec62d18a0b925aeaaa01b81eccf6a16c75ec
Matt Hill <mattnine@protonmail.com>
2025-05-20 22:25:32 -0600

    rename

--- startos/init/taskSetPrimaryUrl.ts
+++ startos/init/taskSetPrimaryUrl.ts
@@ -2,7 +2,7 @@ import { setPrimaryUrl } from '../actions/setPrimaryUrl'
 import { store } from '../fileModels/store.json'
 import { sdk } from '../sdk'
 
-export const setPrimaryUrlTask = sdk.setupOnInit(async (effects) => {
+export const taskSetPrimaryUrl = sdk.setupOnInit(async (effects) => {
   if (!(await store.read((s) => s.GITEA__server__ROOT_URL).const(effects))) {
     await sdk.action.createOwnTask(effects, setPrimaryUrl, 'critical', {
       reason:

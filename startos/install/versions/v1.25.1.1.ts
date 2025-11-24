6bc32dbc4fbd8a17b85007c745ccc2504758c13c
Mariusz Kogen <k0gen@pm.me>
2025-11-24 18:23:20 +0100

    alpha.1 bump

--- startos/install/versions/v1.25.1.1.ts
+++ startos/install/versions/v1.25.1.1.ts
@@ -10,7 +10,7 @@ import { getHttpInterfaceUrls } from '../../utils'
 import { storeJson } from '../../fileModels/store.json'
 
 export const v1_25_1_1 = VersionInfo.of({
-  version: '1.25.1:1-alpha.0',
+  version: '1.25.1:1-alpha.1',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {
